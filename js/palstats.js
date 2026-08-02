23bf594d56448cf1eabd6d7f059b559c488868ff
eyrops-sketch <eyrops@gmail.com>
2026-08-03 00:41:38 +0200

    Update palstats.js

--- js/palstats.js
+++ js/palstats.js
@@ -9,7 +9,10 @@ reproduccion:5,
 ofensiva:8,
 defensa:6,
 movilidad:5,
-plasticidad_ecologica:6
+plasticidad_ecologica:6,
+tamano:9,
+velocidad:6,
+inteligencia:7
 },
 
 "001_02": {
@@ -21,7 +24,10 @@ reproduccion:9,
 ofensiva:2,
 defensa:4,
 movilidad:6,
-plasticidad_ecologica:8
+plasticidad_ecologica:8,
+tamano:4,
+velocidad:5,
+inteligencia:3
 },
 
 "001_03": {
@@ -33,7 +39,10 @@ reproduccion:6,
 ofensiva:6,
 defensa:7,
 movilidad:5,
-plasticidad_ecologica:6
+plasticidad_ecologica:6,
+tamano:7,
+velocidad:4,
+inteligencia:5
 },
 
 "001_04": {
@@ -45,7 +54,10 @@ reproduccion:3,
 ofensiva:10,
 defensa:7,
 movilidad:7,
-plasticidad_ecologica:6
+plasticidad_ecologica:6,
+tamano:10,
+velocidad:8,
+inteligencia:7
 },
 
 "001_05": {
@@ -57,10 +69,168 @@ reproduccion:7,
 ofensiva:4,
 defensa:5,
 movilidad:6,
-plasticidad_ecologica:8
+plasticidad_ecologica:8,
+tamano:5,
+velocidad:6,
+inteligencia:5
+},
+
+"001_06": {
+nombre:"Ditomopyge",
+adaptabilidad:8,
+resistencia:5,
+sociabilidad:4,
+reproduccion:8,
+ofensiva:2,
+defensa:5,
+movilidad:6,
+plasticidad_ecologica:8,
+tamano:3,
+velocidad:5,
+inteligencia:2
+},
+
+"001_07": {
+nombre:"Deinogalerix",
+adaptabilidad:7,
+resistencia:6,
+sociabilidad:3,
+reproduccion:6,
+ofensiva:5,
+defensa:5,
+movilidad:7,
+plasticidad_ecologica:7,
+tamano:5,
+velocidad:7,
+inteligencia:6
+},
+
+"001_08": {
+nombre:"Indricotherium",
+adaptabilidad:6,
+resistencia:10,
+sociabilidad:5,
+reproduccion:3,
+ofensiva:4,
+defensa:9,
+movilidad:4,
+plasticidad_ecologica:5,
+tamano:10,
+velocidad:3,
+inteligencia:5
+},
+
+"001_09": {
+nombre:"Archaeopteryx",
+adaptabilidad:8,
+resistencia:4,
+sociabilidad:5,
+reproduccion:7,
+ofensiva:4,
+defensa:3,
+movilidad:9,
+plasticidad_ecologica:7,
+tamano:2,
+velocidad:8,
+inteligencia:6
+},
+
+"001_10": {
+nombre:"Castoroides",
+adaptabilidad:8,
+resistencia:8,
+sociabilidad:6,
+reproduccion:5,
+ofensiva:4,
+defensa:7,
+movilidad:5,
+plasticidad_ecologica:8,
+tamano:7,
+velocidad:4,
+inteligencia:6
+},
+
+"001_11": {
+nombre:"Graciliceratops",
+adaptabilidad:7,
+resistencia:5,
+sociabilidad:6,
+reproduccion:7,
+ofensiva:4,
+defensa:5,
+movilidad:7,
+plasticidad_ecologica:7,
+tamano:3,
+velocidad:7,
+inteligencia:5
+},
+
+"001_12": {
+nombre:"Hippopotamus",
+adaptabilidad:8,
+resistencia:9,
+sociabilidad:8,
+reproduccion:6,
+ofensiva:7,
+defensa:8,
+movilidad:5,
+plasticidad_ecologica:8,
+tamano:8,
+velocidad:4,
+inteligencia:6
+},
+
+"001_13": {
+nombre:"Nectocaris",
+adaptabilidad:8,
+resistencia:4,
+sociabilidad:5,
+reproduccion:8,
+ofensiva:4,
+defensa:3,
+movilidad:8,
+plasticidad_ecologica:8,
+tamano:2,
+velocidad:7,
+inteligencia:5
+},
+
+"001_14": {
+nombre:"Protosuchus",
+adaptabilidad:8,
+resistencia:7,
+sociabilidad:3,
+reproduccion:7,
+ofensiva:6,
+defensa:6,
+movilidad:7,
+plasticidad_ecologica:8,
+tamano:3,
+velocidad:7,
+inteligencia:5
+},
+
+"001_15": {
+nombre:"Spicomellus",
+adaptabilidad:6,
+resistencia:9,
+sociabilidad:4,
+reproduccion:5,
+ofensiva:5,
+defensa:10,
+movilidad:4,
+plasticidad_ecologica:5,
+tamano:7,
+velocidad:3,
+inteligencia:4
 }
 
 };
+
+
+
+
+
 
 
 
