47edb61b902eed44cda3fc7322208138a3817a16
eyrops-sketch <eyrops@gmail.com>
2026-08-02 19:06:10 +0200

    Update generador.js

--- generador.js
+++ generador.js
@@ -1,1 +1,46 @@
+/*
+========================================================
+GENERADOR PALEONTROPÍA v1.0
+Prueba lectura paleofichas.json
+========================================================
+*/
+
+
+fetch("datos/paleofichas.json")
+
+.then(respuesta => respuesta.json())
+
+.then(datos => {
+
+
+console.log("Fichas cargadas:", datos.length);
+
+
+datos.forEach(ficha => {
+
+console.log(
+ficha.codigo,
+"-",
+ficha.nombre
+);
+
+
+});
+
+
+})
+
+.catch(error => {
+
+console.log(
+"Error cargando JSON:",
+error
+);
+
+});
+
+
+
+
+
 
