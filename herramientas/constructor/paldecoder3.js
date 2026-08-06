219205b5d6d2d98c780d198f400a1f7ab3e95c5d
eyrops-sketch <eyrops@gmail.com>
2026-08-06 13:01:00 +0200

    Update paldecoder3.js

--- herramientas/constructor/paldecoder3.js
+++ herramientas/constructor/paldecoder3.js
@@ -1,1 +1,47 @@
+/*
+========================================================
+PALDECODER3.js v1.0
+Motor de Análisis Estadístico
+PalEntropía
+========================================================
+*/
 
+window.PALDECODER3 = {
+
+    // ==========
+    // NIVEL 1
+    // ==========
+
+    indiceGlobal(){},
+
+    supervivencia(){},
+
+    competencia(){},
+
+    movilidad(){},
+
+    reproduccion(){},
+
+    // ==========
+    // NIVEL 2
+    // ==========
+
+    potencialDepredador(){},
+
+    potencialPresa(){},
+
+    adaptacionAmbiental(){},
+
+    colonizacion(){},
+
+    dominancia(){},
+
+    resiliencia(){},
+
+    // ==========
+    // ANÁLISIS
+    // ==========
+
+    analizar(codigo){}
+
+};
