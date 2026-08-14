c282d769ab762597ea91aef876013e6e0aac54f2
eyrops-sketch <eyrops@gmail.com>
2026-08-14 10:10:02 +0200

    Update cab02.js

--- herramientas/generador/cab02.js
+++ herramientas/generador/cab02.js
@@ -94,21 +94,7 @@ document.addEventListener(
                 ficha.j1
             );
 
-   /* =========================================
-   GEOLOGÍA
-   ========================================= */
 
-if (
-    window.CAB07 &&
-    typeof window.CAB07.procesar ===
-    "function"
-) {
-
-    await window.CAB07.procesar(
-        ficha.j1
-    );
-
-}
 
 
             /* =========================================
